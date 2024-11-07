4f727cd5c6ed554d8a5f9fdcd7832c22b54cb584
AustinMusuya <amusuya57@gmail.com>
2024-11-07 18:26:42 +0300

    cofigure class based view

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import book_list
+from .views import book_list, BookDetailView
 
 urlpatterns = [
-    path('', view=book_list, name='book_list')
+    path('', view=book_list, name='book_list'),
+    path('booklistview', BookDetailView.as_view(), name='booklistview')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
 
@@ -8,3 +9,8 @@ def book_list(request):
     # list of all book instances
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+class BookDetailView(ListView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
