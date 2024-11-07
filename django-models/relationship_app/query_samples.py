7e063ba322d193a1330b6964b824b792fdba30c6
AustinMusuya <amusuya57@gmail.com>
2024-11-07 14:13:13 +0300

    modify query to `Query all books by a specific author` to meet requirments in query_sample.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -7,8 +7,10 @@ Retrieve the librarian for a library.
 from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author.
+author_name = "J.K Rowling"
+author = Author.objects.get(name=author_name)
 
-books_by_author = Book.objects.filter(author__name="J.K. Rowling")
+all_books_by_author = Book.author.all()
 
 
 # List all books in a library.
