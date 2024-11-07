7c62899fae561ac6bb84fa26c53c9ca4d5342aeb
AustinMusuya <amusuya57@gmail.com>
2024-11-07 14:00:43 +0300

    add queries in query_samples.py to `List all books in a library` & `Retrieve the librarian for a library`

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -9,3 +9,12 @@ from .models import Author, Book, Library, Librarian
 # Query all books by a specific author.
 
 books_by_author = Book.objects.filter(author__name="J.K. Rowling")
+
+
+# List all books in a library.
+
+all_books_in_library = Library.books.all()
+
+# Retrieve the librarian for a library.
+
+librarian = Librarian.objects.filter(library__name="National Library")
