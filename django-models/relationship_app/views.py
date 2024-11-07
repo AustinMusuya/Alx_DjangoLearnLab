a4971e718694c8660b597bb07134b12f50a97064
AustinMusuya <amusuya57@gmail.com>
2024-11-07 16:22:26 +0300

    create function based view to render html template with books list

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+
+def book_list(request):
+    # list of all book instances
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
