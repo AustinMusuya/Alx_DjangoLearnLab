e7c0d387daf8858cc03aefef01d6358db5f2552b
AustinMusuya <amusuya57@gmail.com>
2024-11-07 13:18:06 +0300

    add Author model in models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+# class Book(models.Model):
+#     name = models.CharField(max_length=100)
+
+
+# class Library(models.Model):
+#     name = models.CharField(max_length=100)
+
+
+# class Librarian(models.Model):
+#     name = models.CharField(max_length=100)
