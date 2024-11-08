28a15bf193b2e1f3807f592874a4bdd31c1ccc73
AustinMusuya <amusuya57@gmail.com>
2024-11-08 17:55:42 +0300

    configure code in views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,8 +8,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
-# from django.urls import reverse_lazy
-# from django.views.generic import CreateView
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from models import Book
+from forms import BookForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -35,10 +39,44 @@ def register(request):
 # logic view for logout
 
 
-# class SignUpView(CreateView):
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy('login')
-#     template_name = 'registration/register.html'
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
+
+
+def LogoutView(request):
+    return render(request, 'logout.html')
+
+
+def LoginView(request):
+    return render(request, 'login.html')
+
+
+def Register(request):
+    return render(request, 'register.html')
+
+
+class CustomLoginView(LoginView):
+    template_name = 'authentication/login.html'
+    redirect_authenticated_user = True
+
+# Logout view
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'authentication/logout.html'
+
+# Registration view
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'authentication/register.html'
+    success_url = reverse_lazy('login')
+
+
+"UserCreationForm()", "relationship_app/register.html"
 
 
 def list_books(request):
@@ -76,3 +114,42 @@ def librarian_view(request):
 @user_passes_test(lambda user: check_role(user, 'Member'))
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+
+# Add book view
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
+
+# Edit book view
+
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/edit_book.html', {'form': form})
+
+# Delete book view
+
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/delete_book.html', {'book': book})
