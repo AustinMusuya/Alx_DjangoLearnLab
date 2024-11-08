4a894fa07bc9c78197b4acd20c7b3305c9f31f68
AustinMusuya <amusuya57@gmail.com>
2024-11-08 14:38:00 +0300

    configure import statement in urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', view=list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
     path('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
