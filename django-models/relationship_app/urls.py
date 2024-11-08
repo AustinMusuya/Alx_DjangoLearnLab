fe37c96ab7f690e280478260576876aa43df115f
AustinMusuya <amusuya57@gmail.com>
2024-11-08 14:39:14 +0300

    cofigure path for logout in urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
     path('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
