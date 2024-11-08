5ad5307c1fb7ccd7ce32c1372fadb437e38595e4
AustinMusuya <amusuya57@gmail.com>
2024-11-08 14:06:42 +0300

    configure code in views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
