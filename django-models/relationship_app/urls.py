2666078a7808b0f92ff27113816fec1b9b4094ce
AustinMusuya <amusuya57@gmail.com>
2024-11-08 14:36:04 +0300

    create function based view to manually handle registration

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views
 urlpatterns = [
     path('', view=list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
     path('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,21 +4,40 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.contrib import messages
+# from django.urls import reverse_lazy
+# from django.views.generic import CreateView
 
 # Create your views here.
+
+# logic for view register
+
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new user
+            messages.success(request, "Account created successfully!")
+            # Redirect to login page after registration
+            return redirect('login')
+    else:
+        form = UserCreationForm()  # Display an empty form for GET requests
+
+    return render(request, 'registration/register.html', {'form': form})
 
 # logic view for login
 
 # logic view for logout
 
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/register.html'
+# class SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'registration/register.html'
 
 
 def list_books(request):
