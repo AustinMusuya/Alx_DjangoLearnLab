98e4eac840c0a9fef258e094d8459bbc52e55de6
AustinMusuya <amusuya57@gmail.com>
2024-11-08 17:18:55 +0300

    add views to render templates

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 # from django.urls import reverse_lazy
 # from django.views.generic import CreateView
 
@@ -50,3 +51,28 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+def check_role(user, role):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == role
+
+# Admin view
+
+
+@user_passes_test(lambda user: check_role(user, 'Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+
+
+@user_passes_test(lambda user: check_role(user, 'Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+
+
+@user_passes_test(lambda user: check_role(user, 'Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
