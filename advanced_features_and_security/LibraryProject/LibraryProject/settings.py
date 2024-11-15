615ca957327e734741889b319c224b24c9889aa0
AustinMusuya <amusuya57@gmail.com>
2024-11-15 11:53:25 +0300

    add more Secure Headers for deployment environment configuration

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -140,5 +140,6 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-
-# Enforcing Secure Cookie Settings
+# More Secure Headers for deployment environment configuration
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
