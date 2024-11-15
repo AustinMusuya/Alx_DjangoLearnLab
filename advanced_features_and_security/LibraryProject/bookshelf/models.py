cfa901a851a105379ffb48288d87947d2f9d2ebd
AustinMusuya <amusuya57@gmail.com>
2024-11-15 03:52:44 +0300

    add CustomUser model inherits from AbstractUser

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# model for custom user
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
 
 
 class Book(models.Model):
