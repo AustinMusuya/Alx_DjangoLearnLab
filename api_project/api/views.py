3706126dcfd9226d5df0127f29b0b3837c83e09b
AustinMusuya <amusuya57@gmail.com>
2024-11-22 05:25:35 +0300

    create BookList view to pass serialized Book model data to frontend

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework.generics import ListAPIView
 
 # Create your views here.
+
+# view to serialize data from Book Model
+
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
