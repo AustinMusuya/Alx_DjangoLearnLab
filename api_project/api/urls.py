3213d90390d21ae593e49059c55fd745167c51fb
AustinMusuya <amusuya57@gmail.com>
2024-11-22 16:57:16 +0300

    create path url to obtain auth token view post request

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter() # Create an Instance of DefaultRouter Class
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -12,4 +13,7 @@ urlpatterns = [
 
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
+    
+    # path to obtain auth token
+    path('api-token-auth', obtain_auth_token),
 ]
