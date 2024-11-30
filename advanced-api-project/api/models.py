a04bdf33104da653a7e0ed8201898249c7473f52
AustinMusuya <amusuya57@gmail.com>
2024-11-30 06:50:30 +0300

    make model migrations

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    publication_year = models.DateField()
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name="author_books")
