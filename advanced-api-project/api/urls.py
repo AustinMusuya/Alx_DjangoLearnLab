c809fa3b617eedf71e658002bf8332357dcd14c3
AustinMusuya <amusuya57@gmail.com>
2024-11-30 07:05:36 +0300

    add paths to create, update delete and detail view book model

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('books/', views.ListView.as_view(), name='book-list'),
+    path('books/create', views.CreateView.as_view(), name='book-create'),
+    path('books/update', views.UpdateView.as_view(), name='book-update'),
+    path('books/delete', views.DeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
 ]
