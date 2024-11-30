ef9a79b7b959c33f62a7f5010e4bc34807c11607
AustinMusuya <amusuya57@gmail.com>
2024-11-30 07:01:35 +0300

    create views for CRUD operations on the Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
 
 # Create your views here.
+
+"""
+Implement a set of generic views for the Book model to handle CRUD operations. This includes:
+A ListView for retrieving all books.
+A DetailView for retrieving a single book by ID.
+A CreateView for adding a new book.
+An UpdateView for modifying an existing book.
+A DeleteView for removing a book.
+
+"""
+# A ListView for retrieving all books.
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# A DetailView for retrieving a single book by ID.
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# A CreateView for adding a new book.
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# An UpdateView for modifying an existing book.
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# A DeleteView for removing a book.
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
