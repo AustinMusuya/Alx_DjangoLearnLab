74fa25d0856569edd52fd28d59255a6d12746f60
AustinMusuya <amusuya57@gmail.com>
2024-12-05 13:05:25 +0300

    add user_update fbv to handle logic for updating user information.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import login, logout, authenticate
+from .forms import RegisterForm, UpdateForm
 from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
 
@@ -13,3 +16,16 @@ class Register(CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+@login_required(login_url='/login')
+def user_update(request):
+    if request.method == 'POST':
+        form = UpdateForm(request.POST, instance=request.user)  # instantiate the form object
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UpdateForm(instance=request.user) # instantiate the form object
+    return render(request, 'registration/update.html', {'form': form})
+
