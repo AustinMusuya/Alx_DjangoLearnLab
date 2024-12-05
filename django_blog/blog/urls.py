69b69965d4709b1fca5dc9d335d700e7b290c6e7
AustinMusuya <amusuya57@gmail.com>
2024-12-05 13:04:31 +0300

    add path to profile template and path to update user information. include bult in path to accounts from django urls module.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('profile/', views.Home.as_view(), name='profile'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('register/', views.Register.as_view(), name='register'),
+    path('update/', views.user_update, name='update'),
     path('posts/', views.Home.as_view(), name='posts'),
     path('logout/', views.Home.as_view(), name='logout'),
 ]
