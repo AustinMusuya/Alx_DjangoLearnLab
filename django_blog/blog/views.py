a32ab3e6a5c1812fadf7ba4128ba3548c05a5d9f
AustinMusuya <amusuya57@gmail.com>
2024-12-05 05:57:51 +0300

    Import CreateView from djnago views generic model to create CBV 'Register' that handles registration logic for new users.  Import reverse_lazy from django urls module to redirect new user to login route

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .forms import RegisterForm
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 
 class Home(TemplateView):
     template_name = 'blog/base.html'
+
+class Register(CreateView):
+    form_class = RegisterForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
