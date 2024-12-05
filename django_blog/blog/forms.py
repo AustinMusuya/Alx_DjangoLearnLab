62495ddeea8a3b76d36740f3ba72d851db9d9580
AustinMusuya <amusuya57@gmail.com>
2024-12-05 13:03:04 +0300

    create class UpdateForm to allow user to update fields 'username', 'password' and 'email'

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, ReadOnlyPasswordHashField
 from django.contrib.auth.models import User
 
 
@@ -9,3 +9,11 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+# user update form to update username and email fields
+class UpdateForm(forms.ModelForm):
+        password = ReadOnlyPasswordHashField()
+        class Meta:
+            model = User
+            fields = ["username", "email", 'password' ]
