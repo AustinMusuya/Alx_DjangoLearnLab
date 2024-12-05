8401b4d589be7e965cfa418cb489316b84f145ff
AustinMusuya <amusuya57@gmail.com>
2024-12-05 05:54:37 +0300

    configure paths for login and register. Import inbuilt LoginView from django auth views module

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('profile/', views.Home.as_view(), name='profile'),
-    path('login/', views.Home.as_view(), name='login'),
-    path('register/', views.Home.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('register/', views.Register.as_view(), name='register'),
     path('posts/', views.Home.as_view(), name='posts'),
     path('logout/', views.Home.as_view(), name='logout'),
 ]
