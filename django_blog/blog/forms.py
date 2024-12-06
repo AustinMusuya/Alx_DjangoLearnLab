622cc467f4c647bd6d2154bb676951f77d7d5675
AustinMusuya <amusuya57@gmail.com>
2024-12-06 13:45:14 +0300

    create CommentForm for users. configure UpdateForm. import Comment model

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, ReadOnlyPasswordHashField
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 
 class RegisterForm(UserCreationForm):
@@ -14,7 +14,6 @@ class RegisterForm(UserCreationForm):
 
 # user update form to update username and email fields
 class UpdateForm(forms.ModelForm):
-        password = ReadOnlyPasswordHashField()
         class Meta:
             model = User
             fields = ["username", "email", 'password' ]
@@ -23,3 +22,8 @@ class PostForm(forms.ModelForm):
      class Meta:
           model = Post
           fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+     class Meta:
+          model = Comment
+          fields = ['content']
