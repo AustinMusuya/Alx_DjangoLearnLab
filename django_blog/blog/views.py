c4f79c3d38fc2371d01f326cfe2c6cb14fdd0f39
AustinMusuya <amusuya57@gmail.com>
2024-12-06 11:19:10 +0300

    update CRUD class based views to include newly created templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import Post
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
-from .forms import RegisterForm, UpdateForm
+from .forms import RegisterForm, UpdateForm, PostForm
 from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy
 
@@ -13,29 +13,54 @@ from django.urls import reverse_lazy
 class Home(TemplateView):
     template_name = 'blog/base.html'
 
-class ListBlog(ListView):
+class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
+    template_name = 'blog/list-post.html'
     
 
-class UpdateBlog(UpdateView):
+class PostUpdateView(UpdateView):
     model = Post
     fields = ['title', 'content']
+    template_name = 'blog/update-post.html'
 
-class DeletBlog(DeleteView):
+class PostDeleteView(DeleteView):
     model = Post
     success_url = reverse_lazy("/")
+    template_name = 'blog/delete-post.html'
 
-class CreateBlog(CreateView):
+class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content']
+    template_name = 'blog/create-post.html'
 
-class DetailBlog(DetailView):
-    pass
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail-post.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+
 class Register(CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+@login_required(login_url='/login')
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/')
+    else:
+        form = PostForm()
+        return render(request, 'blog/create-post.html', {'form': form})
 
 @login_required(login_url='/login')
 def user_update(request):
