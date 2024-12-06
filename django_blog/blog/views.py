f8a56a6294ca772f22d0369481fba8f3ac75de6d
AustinMusuya <amusuya57@gmail.com>
2024-12-06 13:16:46 +0300

    make changes in PostDetailView, PostCreateView & PostDeleteView to handle CRUD logic

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -33,27 +33,35 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = reverse_lazy("post_list")
-    template_name = 'blog/post_confrim_delete.html'
+    template_name = 'blog/post_confirm_delete.html'
 
     def test_func(self):
         # Allow only the author of the post to delete it
         post = self.get_object()  # Get the current post instance
         return self.request.user == post.author
 
-class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'blog/post_form.html'
 
-    def test_func(self):
-        # Allow only the author of the post to delete it
-        post = self.get_object()  # Get the current post instance
-        return self.request.user == post.author
+    def form_valid(self, form):
+        # Set the author to the currently logged-in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        # Redirect to the list view after successful creation
+        return reverse_lazy('post-list')
 
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
