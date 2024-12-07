ae9015182131e309e4a10f37790060b08700f8c3
AustinMusuya <amusuya57@gmail.com>
2024-12-07 15:41:21 +0300

    add dunder string method for Post model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User , on_delete=models.CASCADE, related_name='author_posts')
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments')
