7caa9231a444e20a641403897ec630900a61d2e6
AustinMusuya <amusuya57@gmail.com>
2024-12-10 12:10:43 +0300

    add directory to accounts app templates

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'social_media_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
