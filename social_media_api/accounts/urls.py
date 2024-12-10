4a1917bdb5686efa3e75ae138e70f50bf13280dc
AustinMusuya <amusuya57@gmail.com>
2024-12-10 16:07:37 +0300

    import UserViewSet from views import DefaultRouter from rest framework routers and create class instance. include router urls.

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('users',UserViewSet)
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
@@ -8,4 +13,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+
+    # viewset for router urls
+    path('api/', include(router.urls))
 ]
