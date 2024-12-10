9e3a22b34ca05dc986daf6f69a630cd2e10094c8
AustinMusuya <amusuya57@gmail.com>
2024-12-10 12:13:01 +0300

    create view to handle logic for rendering home template

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+# home view
+class HomeView(TemplateView):
+    template_name = 'accounts/home.html'
