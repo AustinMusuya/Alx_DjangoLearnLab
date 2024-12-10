bcb8abaea2323ccf39bdd608193ab07bda562554
AustinMusuya <amusuya57@gmail.com>
2024-12-10 13:28:50 +0300

    create custom user class that extends AbstractUser to include profile_picture, bio and followers fields

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField()
+    profile_picture = models.ImageField()
+    followers = models.ManyToManyField("self", symmetrical=False, related_name='following')
+
+    def __str__(self):
+        return self.username
