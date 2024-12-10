0e4ff3b49fa4132d43e57a943dbe0977266dd97c
AustinMusuya <amusuya57@gmail.com>
2024-12-10 15:06:04 +0300

    create registration view

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 # home view
 class HomeView(TemplateView):
     template_name = 'accounts/home.html'
+
+
+
+class RegistrationView(CreateView):
+    form_class = RegisterForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+class ProfileView(TemplateView, LoginRequiredMixin):
+    template_name = 'accounts/profile.html'
