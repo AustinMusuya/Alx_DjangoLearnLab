6653a1667ab4116a014b5fd5f8693983ac2b39e5
AustinMusuya <amusuya57@gmail.com>
2024-12-13 12:01:53 +0300

    add api routes for obtain tokens ,login and registration

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,16 +3,20 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import UserViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import ObtainAuthToken
 
 router = DefaultRouter()
-router.register('users',UserViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('register/', views.RegistrationView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name = 'registration/login.html'), name='login'),
+    path('api/register/', views.RegistrationAPIView.as_view(), name='api-register'),
+    path('api/login/', views.LoginAPIView.as_view(), name='api-login'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('api-token-auth/', ObtainAuthToken.as_view(), name='api-token-auth'),
 
     # viewset for router urls
     path('api/', include(router.urls))
