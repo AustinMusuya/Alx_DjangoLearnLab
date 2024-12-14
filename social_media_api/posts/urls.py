2d873e0c7aa59c41de0cf9ccc40e4f6f961c6dd3
AustinMusuya <amusuya57@gmail.com>
2024-12-14 15:53:28 +0300

    configure urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
      path('', include(routers.urls)),
      path('feed/', views.UserFeedView.as_view(), name='user-feed'),
      path('posts/<int:pk>/like/', views.LikedPostView.as_view(), name='like-post'),
-    path('posts/<int:pk>/unlike/', views.UnlikePostView.as_view(), name='unlike-post'),
+     path('posts/<int:pk>/unlike/', views.UnlikePostView.as_view(), name='unlike-post'),
 ]
