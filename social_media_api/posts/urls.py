09af420d419158493fad89152086ddfb9a79c821
AustinMusuya <amusuya57@gmail.com>
2024-12-14 15:53:00 +0300

    include path urls to likepost and unlike post views

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,4 +9,6 @@ routers.register('comment-view',views.CommentViewSet )
 urlpatterns = [
      path('', include(routers.urls)),
      path('feed/', views.UserFeedView.as_view(), name='user-feed'),
+     path('posts/<int:pk>/like/', views.LikedPostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', views.UnlikePostView.as_view(), name='unlike-post'),
 ]
